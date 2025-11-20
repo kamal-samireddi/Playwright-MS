0424a055266e19fba1297b69f6ae13992dae1e35
kamal-samireddi <kamal.samireddi@gmail.com>
2025-11-20 19:41:00 +0000

    initial commit

---
+++ tests/search/search-basic.spec.js
@@ -0,0 +1,8 @@
+﻿const { test, expect } = require('@playwright/test');
+
+test.describe('Search - Basic Functionality', () => {
+  test('example test @smoke @sanity @regression', async ({ page }) => {
+    await page.goto('https://www.marksandspencer.com');
+    await expect(page).toHaveTitle(/Marks & Spencer/);
+  });
+});
