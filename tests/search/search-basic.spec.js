6f0598108a22e6a97a7d017bbfcf1a718c0536ba
srikanth motamarri <srikanthmsd07@gmail.com>
2026-02-28 19:45:46 +0530

    initial setup

--- tests/search/search-basic.spec.js
+++ tests/search/search-basic.spec.js
@@ -1,8 +1,11 @@
-﻿const { test, expect } = require('@playwright/test');
+﻿const { test, expect } = require("@playwright/test");
 
-test.describe('Search - Basic Functionality', () => {
-  test('example test @smoke @sanity @regression', async ({ page }) => {
-    await page.goto('https://www.marksandspencer.com');
+test.describe("Search - Basic Functionality", () => {
+  test("example test @smoke @sanity @regression", async ({ page }) => {
+    await page.goto("https://www.marksandspencer.com", {
+      waitUntil: "domcontentloaded",
+      timeout: 30000,
+    });
     await expect(page).toHaveTitle(/Marks & Spencer/);
   });
 });
